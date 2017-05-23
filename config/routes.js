224b156f483f6d481659de7cee4e2bcf9018a898
Lior Chamla <lchamla@gmail.com>
2017-05-23 22:22:10 +0000

    finishing route file

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,26 @@
+/**
+ * Fichier de gestion des routes de l'application !
+ */
 const express = require('express');
-const PageController = require('../controllers/PageController');
 const router = express.Router();
+// Controllers :
+const PageController = require('../controllers/PageController'); // Demo controller qui gère les pages
 
+/**
+ * Front-end routes
+ */
 router.get('/', PageController.home);
+router.get('/posts', PageController.index);
+router.get('/post/:slug', PageController.show);
+router.get('/posts/create', PageController.form);
+router.post('/post/', PageController.create);
+
+/**
+ * Admin routes
+ */
+
+/**
+ * API Routes
+ */
 
 module.exports = router;
