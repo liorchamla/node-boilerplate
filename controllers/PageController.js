981d195e07228130e3933a21124967b726317bef
Lior Chamla <lchamla@gmail.com>
2017-05-23 22:51:39 +0000

    finilizing Pages demo controller

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -1,3 +1,58 @@
+/**
+ * Controller de démo : Pages - gestion des pages
+ */
+
+const mongoose = require('mongoose');
+const Page = mongoose.model('Page');
+
+/**
+ * Affichage de la homepage
+ */
 exports.home = (req, res, next) => {
-    res.render('homepage.pug', {title: "Home", herotitle: "Lior's Boilerplate", herosubtitle: "Node, Express, Pug, Webpack, Bulma"});
+    res.render('homepage', {
+        title: "Home", 
+        herotitle: "Lior's Boilerplate", 
+        herosubtitle: "Node, Express, Pug, Webpack, Bulma"
+    });
+}
+
+/**
+ * Affichage du formulaire d'ajout de Page
+ */
+exports.form = (req, res, next) => {
+    res.render('pages/form', {
+        title: "Créer une nouvelle page", 
+        subtitle: "Remplissez le formulaire ci-dessous"
+    });
+}
+
+/**
+ * Affichage de la liste des pages
+ */
+exports.index = async (req, res, next) => {
+    const pages = await Page.find();
+    res.render('pages/index', {
+        title: "Liste des pages", 
+        pages
+    });
+}
+
+/**
+ * Affichage d'une page en particulier
+ */
+exports.show = async (req, res, next) => {
+    const page = await Page.findOne({slug: req.params.slug});
+    res.render('pages/show', {
+        title: page.title, 
+        page
+    });
+}
+
+/**
+ * Création d'une page (via formulaire POST)
+ */
+exports.create = async (req, res, next) => {
+    const page = new Page(req.body);
+    await page.save();
+    res.redirect(`/post/${page.slug}`);
 }
